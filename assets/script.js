f6890c451c04331b01c3efa6eddbbce9cdf5c3f5
Pegasoos <circumambulation24@yahoo.com>
2021-08-31 12:06:24 -0400

    Search History Buttons and Functionality

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,22 @@
 
 //create search hitory element on each search that get same results as original search
+//search history renders buttons in #search-history div
+function searchHistory(){
+    var cityName = $(".weather-search").val();
+    var historyButton = $("<button>").val(cityName);
+    $(historyButton).text($(historyButton).val());
+    $(historyButton).addClass("weather-button");
+    $(historyButton).attr("id", "history-search")
+    var searchHistoryDiv = $("#search-history")
+    $(historyButton).on("click", weatherSearch)
+    $(searchHistoryDiv).append(historyButton);
+}
+
+//use weather search class to trigger search on both original search and generated buttons, above code to synchronize button text and code
+//weather-button, weather-search now classes for button generation
 function generateResults(response){
-    for(i=0;i<40;i+=8){ 
-        // create $("<div>").addClass("col-md-2").append(all the weather info)
+    
+    for(i=0;i<40;i+=8){
         var fiveDayDiv = $("#five-day")
         var startDate = parseInt(response.list[i].dt);
         var newDate = new Date(startDate * 1000).toLocaleDateString("en-US");
@@ -19,11 +33,18 @@ function generateResults(response){
  function weatherSearch(){
      //clears results display
     $("#currentForecast").empty()
-
-    var cityName = $("#weather-search").val();
+    var fiveDayDiv = $("#five-day")
+    $(fiveDayDiv).empty();
+    //cityName value gotten in different ways depending on whether a search history button or typed search was used
+    if($(this).is("#history-search")){
+        var cityName = $(this).val();
+    }
+    else{
+        var cityName = $(".weather-search").val();
+    }
     
     var forecastQueryURL = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=9e04f2b6c335750c8cfb2144be29b60f`
-    
+
     $.ajax({
         url: forecastQueryURL,
         method: "GET"
@@ -32,7 +53,6 @@ function generateResults(response){
         //try passing response to function that handles loop?
         generateResults(response)
            
-        
         //retrieve lat and lon from 5 day to do one call search for uv data
         var searchLon = JSON.stringify(response.city.coord.lon)
         var searchLat = JSON.stringify(response.city.coord.lat)
@@ -54,7 +74,9 @@ function generateResults(response){
             $("#currentForecast").append(resultHeader).append(currentTemp).append(currentWindSpeed).append(currentHumidity).append(currentUVI)
         })
     })
-    
+    //condition to prevent search history buttons from replicating themselves
+    if($(this).is("#search-button")){
+    searchHistory();
+    }
 }
-//cors fix C:\Program Files (x86)\Google\Chrome\Application, chrome.exe --disable-web-security --user-data-dir=c:\my-chrome-data\data
-$("#weather-button").on("click", weatherSearch)
+$(".weather-button").on("click", weatherSearch)
